57494699e1c9601f699a71987dcc8c9f1f700598
madhav <smadhvendra20@gmail.com>
2024-12-15 18:31:26 +0530

    Use react-scroll Link o wrap the hire btn

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./intro.css";
 import introImg from "../../assets/image.png";
 import hireImg from "../../assets/hireme.png";
+import { Link } from "react-scroll";
 
 const Intro = () => {
   return (
@@ -18,10 +19,12 @@ const Intro = () => {
             <p className="introPara">
                 I have experience building responsive front-ends of websites which are intuitive and visually calm.
             </p>
-            <button className="introHireBtn">
-                <img src={hireImg} alt="" className="hireImg" />
-                <span>Hire me</span>
-            </button>
+            <Link>
+                <button className="introHireBtn">
+                    <img src={hireImg} alt="" className="hireImg" />
+                    <span>Hire me</span>
+                </button>
+            </Link>
         </div>
 
         <img src={introImg} alt="My photo" className="introImg" />
