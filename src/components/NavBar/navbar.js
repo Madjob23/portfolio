b239308ec09169bb6c20079036e0d1f47d17946b
madhav <smadhvendra20@gmail.com>
2024-12-17 00:48:56 +0530

    Hail mary attempt at responsiveness

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -14,10 +14,10 @@ const Navbar = () => {
     <nav className="navbar">
       <img src={logo} alt="logo" className='navLogo'/>
       <div className="navLinks">
-        <Link to="home" className="navLink">Home</Link>
+        <Link to="intro" className="navLink">Home</Link>
         <Link to="about" className="navLink">About</Link>
-        <Link to="projects" className="navLink">Projects</Link>
-        <Link to="contact" className="navLink">Internships</Link>
+        <Link className="navLink">Projects</Link>
+        <Link className="navLink">Internships</Link>
       </div>
       <button className="navContactBtn">
         <img src={contactImg} alt="Contact icon" className="contactImg" />
